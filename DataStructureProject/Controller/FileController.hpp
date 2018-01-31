f36b668346f1b8e9ea06d5f3b2020f1fa6ef83c2
kuojsean <kuo.j.sean@gmail.com>
2018-01-31 08:45:52 -0700

    Created the header for the filecontroller.

--- DataStructureProject/Controller/FileController.hpp
+++ DataStructureProject/Controller/FileController.hpp
@@ -9,6 +9,19 @@
 #ifndef FileController_hpp
 #define FileController_hpp
 
-#include <stdio.h>
+#include "../Data/CrimeData.hpp"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+class FileController
+{
+public:
+    static vector<CrimeData> readeCrimeDataToVector(string filename);
+}
 
 #endif /* FileController_hpp */
