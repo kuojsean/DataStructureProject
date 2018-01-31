24030dbf21f620491775d701f433c6deb14347e1
kuojsean <kuo.j.sean@gmail.com>
2018-01-31 08:01:52 -0700

    Created all the functions for the timer.

--- DataStructureProject/Controller/Tools/Timer.cpp
+++ DataStructureProject/Controller/Tools/Timer.cpp
@@ -20,3 +20,24 @@ void Timer :: resetTimer()
     executionTime = 0;
 }
 
+void Timer :: startTimer()
+{
+    executionTime = clock();
+}
+
+void Timer :: stopTimer()
+{
+    assert(executionTime != 0);
+    executionTime = clock() - executionTime;
+}
+
+void Timer :: displayInformation()
+{
+    cout << "The execution time is: " << executionTime << endl;
+    cout << "In human time it is " << double (executionTime)/CLOCKS_PER_SEC << " seconds" << endl;
+}
+
+long Timer :: getTimeInMicroseconds()
+{
+    return executionTime;
+}
