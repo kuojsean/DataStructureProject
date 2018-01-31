864b51131886bbb03c16a963f52280f24939922d
kuojsean <kuo.j.sean@gmail.com>
2018-01-31 07:56:42 -0700

    Finished all the components in the timer header and created the timer and reset method in the cpp.

---
+++ DataStructureProject/Controller/Tools/Timer.cpp
@@ -0,0 +1,22 @@
+//
+//  Timer.cpp
+//  DataStructureProject
+//
+//  Created by Kuo, Sean on 1/31/18.
+//  Copyright © 2018 Kuo, Sean. All rights reserved.
+//
+
+#include "Timer.hpp"
+
+using namespace std;
+
+Timer :: Timer()
+{
+    executionTime = 0;
+}
+
+void Timer :: resetTimer()
+{
+    executionTime = 0;
+}
+
