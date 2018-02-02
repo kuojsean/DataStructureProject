23b842918f20de050e0052002ee6b891bdd60802
kuojsean <kuo.j.sean@gmail.com>
2018-02-02 07:58:52 -0700

    Created the loop to search for the max and minimum while using a timer.

--- DataStructureProject/Controller/Controller.cpp
+++ DataStructureProject/Controller/Controller.cpp
@@ -12,6 +12,7 @@ using namespace std;
 
 void Controller :: start()
 {
+    findMaxAndMin();
     cout << "Testing the Timer class" << endl;
     Timer codeTimer;
     codeTimer.startTimer();
@@ -26,5 +27,27 @@ void Controller :: start()
 
 void Controller :: findMaxAndMin()
 {
+    Timer searchTimer;
+    searchTimer.startTimer();
     vector<CrimeData> myData = FileController :: readCrimeDataToVector("/Users/skuo7320/Documents/DataStructures2018/DataStructureProject/DataStructureProject/Data/crime.csv");
+    
+    int minIndex = 0;
+    int maxIndex = 0;
+    
+    for (int index = 0; index < myData.size(); index++)
+    {
+        if (myData [minIndex] < myData [index])
+        {
+            minIndex = index;
+        }
+        
+        if (myData [maxIndex] > myData[index])
+        {
+            maxIndex = index;
+        }
+    }
+    searchTimer.stopTimer();
+    cout << " The smallest crime stat is at "<< minIndex << " and it is: " << myData[minIndex] << endl;
+    cout << " The largest crime stat is at "<< maxIndex << " and it is: " << myData[maxIndex] << endl;
+    searchTimer.displayInformation();
 }
