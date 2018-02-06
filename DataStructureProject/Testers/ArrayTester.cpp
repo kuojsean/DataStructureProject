acd57c165a12f3fc65048a1ae3a92b91b275a9d5
kuojsean <kuo.j.sean@gmail.com>
2018-02-06 08:35:18 -0700

    Started the array tester class and header with the class definition.

---
+++ DataStructureProject/Testers/ArrayTester.cpp
@@ -0,0 +1,9 @@
+//
+//  ArrayTester.cpp
+//  DataStructureProject
+//
+//  Created by Kuo, Sean on 2/6/18.
+//  Copyright © 2018 Kuo, Sean. All rights reserved.
+//
+
+#include "ArrayTester.hpp"
---
+++ DataStructureProject/Testers/ArrayTester.hpp
@@ -0,0 +1,24 @@
+//
+//  ArrayTester.hpp
+//  DataStructureProject
+//
+//  Created by Kuo, Sean on 2/6/18.
+//  Copyright © 2018 Kuo, Sean. All rights reserved.
+//
+
+#ifndef ArrayTester_hpp
+#define ArrayTester_hpp
+
+#include <iostream>
+#include "../Model/Structures/Array.hpp"
+#include "../Controller/FileController.hpp"
+#include "../Controller/Tools/Timer.hpp"
+
+class ArrayTester
+{
+public:
+    void testArrayUse();
+    void testAdvancedArray();
+};
+
+#endif /* ArrayTester_hpp */
