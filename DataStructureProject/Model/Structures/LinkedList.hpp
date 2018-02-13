dcc9fea56ec65a32d8cd09c967efcef2dfd2871a
kuojsean <kuo.j.sean@gmail.com>
2018-02-13 07:29:30 -0700

    Started the linked list.

---
+++ DataStructureProject/Model/Structures/LinkedList.hpp
@@ -0,0 +1,13 @@
+//
+//  LinkedList.hpp
+//  DataStructureProject
+//
+//  Created by Kuo, Sean on 2/12/18.
+//  Copyright © 2018 Kuo, Sean. All rights reserved.
+//
+
+#ifndef LinkedList_hpp
+#define LinkedList_hpp
+
+
+#endif /* LinkedList_hpp */
