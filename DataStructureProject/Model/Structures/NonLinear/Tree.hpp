11d7c2990488a7063d575459eeb1ee9625c3b03b
kuojsean <kuo.j.sean@gmail.com>
2018-03-28 07:31:59 -0600

    started the tree header file.

---
+++ DataStructureProject/Model/Structures/NonLinear/Tree.hpp
@@ -0,0 +1,13 @@
+//
+//  Tree.hpp
+//  DataStructureProject
+//
+//  Created by Kuo, Sean on 3/27/18.
+//  Copyright © 2018 Kuo, Sean. All rights reserved.
+//
+
+#ifndef Tree_h
+#define Tree_h
+
+
+#endif /* Tree_h */
